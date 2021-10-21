bc19f0168a6aa013d684f332999a253a277ff78b
yong.ma <maywide@qq.com>
2021-10-21 14:19:39 +0800

    use RevisedSimplex by default

--- src/main/java/com/github/cloudecho/bnb/SimplexFactory.java
+++ src/main/java/com/github/cloudecho/bnb/SimplexFactory.java
@@ -6,7 +6,7 @@ public class SimplexFactory {
     }
 
     public enum SimplexType {
-        NORMAL, BIG, REVISED;
+        REVISED, NORMAL, BIG;
 
         static SimplexType of(String name) {
             for (SimplexType t : values()) {
@@ -19,7 +19,7 @@ public class SimplexFactory {
     }
 
     public static final String SIMPLEX_TYPE_PROP = "com.github.cloudecho.bnb.SIMPLEX_TYPE";
-    static final SimplexType SIMPLEX_TYPE = SimplexType.of(System.getProperty(SIMPLEX_TYPE_PROP, ""));
+    static final SimplexType SIMPLEX_TYPE = SimplexType.of(System.getProperty(SIMPLEX_TYPE_PROP, "REVISED"));
 
     public static Simplex newSimplex(double[] c, double[][] a, double[] b) {
         return newSimplex(SIMPLEX_TYPE, c, a, b);
@@ -30,9 +30,9 @@ public class SimplexFactory {
             case BIG:
                 return new BigSimplex(c, a, b);
             case REVISED:
+            default:
                 return new RevisedSimplex(c, a, b);
             case NORMAL:
-            default:
                 return new Simplex(c, a, b);
         }
     }
